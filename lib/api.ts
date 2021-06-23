6e48483d9e366fd367cb2b3c6c3a95fffe9fc5cf
Helder Ferreira <me@helderferreira.io>
2021-06-23 18:04:01 +0100

    add default nextjs -> Ghost API querys

--- lib/api.ts
+++ lib/api.ts
@@ -1,47 +1,76 @@
-import fs from 'fs'
-import { join } from 'path'
-import matter from 'gray-matter'
+import GhostContentAPI from '@tryghost/content-api';
+import { GHOST_API_URL_DEFAULT, GHOST_API_KEY_DEFAULT } from './defaults';
 
-const postsDirectory = join(process.cwd(), '_posts')
+const GHOST_API_URL = process.env.GHOST_API_URL || GHOST_API_URL_DEFAULT
+const GHOST_API_KEY = process.env.GHOST_API_KEY || GHOST_API_KEY_DEFAULT
 
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory)
-}
+const api = new GhostContentAPI({
+  url: GHOST_API_URL,
+  key: GHOST_API_KEY,
+  version: 'v3',
+})
 
-export function getPostBySlug(slug: string, fields: string[] = []) {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
+const is404 = (error) => /not found/i.test(error.message)
 
-  type Items = {
-    [key: string]: string
+export async function getPreviewPostBySlug(slug) {
+  const params = {
+    slug,
+    fields: 'slug',
+    limit: 'all',
   }
 
-  const items: Items = {}
-
-  // Ensure only the minimal needed data is exposed
-  fields.forEach((field) => {
-    if (field === 'slug') {
-      items[field] = realSlug
-    }
-    if (field === 'content') {
-      items[field] = content
-    }
-
-    if (data[field]) {
-      items[field] = data[field]
-    }
-  })
-
-  return items
+  try {
+    const post = await api.posts.read(params)
+    return post
+  } catch (error) {
+    // Don't throw if an slug doesn't exist
+    if (is404(error)) return
+    throw error
+  }
 }
 
-export function getAllPosts(fields: string[] = []) {
-  const slugs = getPostSlugs()
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+export async function getAllPostsWithSlug() {
+  const params = {
+    fields: 'slug',
+    limit: 'all',
+  }
+  const posts = await api.posts.browse(params)
   return posts
+}
+
+export async function getAllPostsForHome(preview) {
+  const params = {
+    limit: 'all',
+    include: 'authors',
+    order: 'published_at DESC',
+    ...(preview && { status: 'all' }),
+  }
+  const posts = await api.posts.browse(params)
+  return posts
+}
+
+export async function getPostAndMorePosts(slug, preview) {
+  const singleObjectParams = {
+    slug,
+    include: 'authors',
+    ...(preview && { status: 'all' }),
+  }
+  const moreObjectParams = {
+    limit: 3,
+    include: 'authors',
+    ...(preview && { status: 'all' }),
+  }
+  const post = await api.posts.read(singleObjectParams).catch((error) => {
+    // Don't throw if an slug doesn't exist
+    if (is404(error)) return
+    throw error
+  })
+  const morePosts = (await api.posts.browse(moreObjectParams))
+    ?.filter(({ slug }) => post.slug !== slug)
+    .slice(0, 2)
+
+  return {
+    post,
+    morePosts,
+  }
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,4 @@
-export const BLOG_NAME = 'HF\'s blog';
-export const GITHUB_PROFILE = 'https://github.com/HelderFSFerreira';
+export const BLOG_NAME:string = 'HF\'s blog';
+export const GITHUB_PROFILE:string = 'https://github.com/HelderFSFerreira';
+export const CMS_URL:string = 'https://ghost.org/';
+export const HOME_OG_IMAGE_URL:string = 'https://og-image.vercel.app/Next.js%20Example%20Blog%20with%20**Ghost**.png?theme=light&md=1&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fghost.org%2Fimages%2Flogo.svg';
